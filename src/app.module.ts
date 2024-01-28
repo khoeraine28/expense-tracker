0d168b6dfa3096a1b6bf5a1dce944f71f4633b3b
Wendell Soriano <dhey28@gmail.com>
2024-01-29 04:43:21 +0800

    add stores module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PurchasesModule } from './purchases/purchases.module';
 import { ProductsModule } from './products/products.module';
+import { StoresModule } from './stores/stores.module';
 
 @Module({
-  imports: [UsersModule, PurchasesModule, ProductsModule],
+  imports: [UsersModule, PurchasesModule, ProductsModule, StoresModule],
   controllers: [],
   providers: [],
 })
