6a4443c96584388f331eedb250006466d997908b
Wendell Soriano <dhey28@gmail.com>
2024-01-29 04:42:14 +0800

    add purchases module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { PurchasesModule } from './purchases/purchases.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PurchasesModule],
   controllers: [],
   providers: [],
 })
