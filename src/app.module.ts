5b401606a286f75a622a19977e787c04d6abd6bc
Wendell Soriano <dhey28@gmail.com>
2024-01-29 04:51:42 +0800

    add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { PurchasesModule } from './purchases/purchases.module';
 import { ProductsModule } from './products/products.module';
 import { StoresModule } from './stores/stores.module';
 import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, PurchasesModule, ProductsModule, StoresModule, CategoriesModule],
+  imports: [
+    UsersModule,
+    PurchasesModule,
+    ProductsModule,
+    StoresModule,
+    CategoriesModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
